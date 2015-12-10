728ef69c97d59e92a3db25a9cddb4f862df4669d
rohan.prinja <rohan.prinja@samsung.com>
2015-12-10 16:42:00 +0900

    add and sub for point

--- src/primitives/point.rs
+++ src/primitives/point.rs
@@ -1,6 +1,7 @@
 use std::clone::Clone;
 use std::cmp::{Eq, Ordering, PartialEq, PartialOrd};
 use std::fmt::{Display, Formatter, Result};
+use std::ops::{Add, Sub};
 
 #[derive(Debug)]
 pub struct Point {
@@ -64,9 +65,27 @@ impl Clone for Point {
     }
 }
 
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self {
+        Point::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Self) -> Self {
+        Point::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::ops::{Add, Sub};
     use super::Point;
+
     #[test]
     fn test_eq() {
         assert_eq!(Point::new(2f64, -9.3f64), Point::new(2f64, -9.3f64));
@@ -75,5 +94,23 @@ mod test {
     #[test]
     fn test_lt() {
         assert!(Point::new(2f64, 9.3f64) < Point::new(3f64, -9.3f64));
+    }
+
+    #[test]
+    fn test_add() {
+        let p1 = Point::new(14.01f64, -3.2f64);
+        let p2 = Point::new(-14.01f64, 5f64);
+        let p3 = p1 + p2;
+        assert_eq!(p3.x, 0f64);
+        assert!(p3.y - 1.8f64 < 1e-5); // floating point errors
+    }
+
+    #[test]
+    fn test_sub() {
+        let p1 = Point::new(3.01f64, 3.2f64);
+        let p2 = Point::new(3f64, 5f64);
+        let p3 = p1 - p2;
+        assert!(p3.x - 0.01f64 < 1e-5); // floating point errors
+        assert!(p3.y + 1.8f64 < 1e-5); // floating point errors
     }
 }
